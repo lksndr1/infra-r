bbe9f33de96d53d85294ece99936244af6ecd5ab
hettfield git <olekpl90@gmail.com>
2024-12-19 13:57:39 +0100

    time

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import logo from '/infra-logo.jpg'
+import { useState } from 'react'
 
 export default function Header() {
-    const [now, setNow] = useState[newDate]
+    const [now, setNow] = useState(new Date())
+
+    setInterval(() => setNow(new Date()), 1000)
 
     return (
         <header>
