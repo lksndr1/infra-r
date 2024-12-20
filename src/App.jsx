39fc465bbcf2136b233c7a7aa49ed176f98c4a1a
hettfield git <olekpl90@gmail.com>
2024-12-20 13:07:45 +0100

    tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import Header from './components/Header'
 import FaqsSection from './components/FaqsSection'
 import ValuesSection from './components/ValuesSection'
+import HeadingSection from './components/HeadingSection'
+import TabsSection from './components/TabsSection'
+import ContactUsSection from './components/ContactUsSection'
+import { useState } from 'react'
 
 export default function App() {
+  const [tab, setTab] = useState('contactus')
+
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <FaqsSection />
-        <ValuesSection />
+        <HeadingSection />
+
+        <TabsSection active={tab} onChange={(current) => setTab(current)} />
+
+        {tab === 'main' && (
+          <>
+            <FaqsSection />
+            <ValuesSection />
+          </>
+        )}
+
+        {tab === 'contactus' && <ContactUsSection />}
       </main>
-    </div >
+    </>
   )
 }
---
+++ src/components/HeadingSection.jsx
@@ -0,0 +1,8 @@
+export default function HeadingSection() {
+    return (
+        <section>
+            <h1 className='centered'>Inframet</h1>
+            <h3 className='centered' style={{ color: '#796' }}>Energy-efficient solutions</h3>
+        </section>
+    )
+}
