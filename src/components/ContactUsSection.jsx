d1d27e77cc7f29c37e4b5cf3b74b91a2d38163cd
hettfield git <olekpl90@gmail.com>
2024-12-21 10:15:53 +0100

    feat/form

--- src/components/ContactUsSection.jsx
+++ src/components/ContactUsSection.jsx
@@ -1,7 +1,47 @@
+import Button from './Button/Button'
+import { useState } from 'react'
+
 export default function ContactUsSection() {
+    const [name, setName] = useState('')
+    const [theme, setTheme] = useState('help')
+    const [hasError, setHasError] = useState(false)
+
+    function handleNameChange(event) {
+        setName(event.target.value)
+        setHasError(event.target.value.trim().length === 0)
+    }
+
+    function handleThemeChange(event) {
+        setTheme(event.target.value)
+    }
+
     return (
         <section>
             <h3>Contact Us</h3>
+
+            <form>
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    className="control"
+                    value={name}
+                    style={{
+                        border: hasError ? '2px solid red' : null,
+                    }}
+                    onChange={handleNameChange}
+                />
+
+                <label htmlFor="theme">Theme:</label>
+                <select id="theme" className="control" value={theme} onChange={handleThemeChange}>
+                    <option value="wishes">Wishes</option>
+                    <option value="help">Help</option>
+                    <option value="partnership">Parthership</option>
+                    <option value="other">Other</option>
+                </select>
+
+                <Button disabled={hasError} isActive={!hasError}>Sent</Button>
+            </form>
         </section>
     )
 }
