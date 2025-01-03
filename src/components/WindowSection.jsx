706d6fe361d9c3d9dceaf9a5d9152cc285cc1954
hettfield git <olekpl90@gmail.com>
2025-01-03 15:12:24 +0100

    users

--- src/components/WindowSection.jsx
+++ src/components/WindowSection.jsx
@@ -1,9 +1,23 @@
 import Button from './Button/Button'
 import Modal from './Modal/Modal'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function WindowSection() {
     const [isModalOpen, setIsModalOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [users, setUsers] = useState([])
+
+    async function fetchUsers() {
+        setLoading(true)
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users = await response.json()
+        setUsers(users)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
 
     function openModal() {
         setIsModalOpen(true)
@@ -13,13 +27,23 @@ export default function WindowSection() {
         <section>
             <h3>Window</h3>
 
-            <Button buttonClicked={openModal}>Show info</Button>
+            <Button style={{ marginBottom: '1rem' }} buttonClicked={openModal}>Show info</Button>
 
             <Modal open={isModalOpen}>
                 <h3>Info</h3>
                 <p>Inframet-r is...</p>
                 <Button buttonClicked={() => setIsModalOpen(false)}>Close window</Button>
             </Modal>
-        </section>
+
+            {loading && <p>Loading...</p>}
+
+            {
+                !loading && <ul>
+                    {users.map((user) => (
+                        <li key={user.id}>{user.name}</li>
+                    ))}
+                </ul>
+            }
+        </section >
     )
 }
